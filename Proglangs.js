4a1c8178cf6306e1ee9ae64af87aaa638cd6f0b0
Sondre Søbye <Sondre.sobye@gmail.com>
2013-10-10 07:56:12 +0200

    Alle filene fra Lab-oppgavene
    Denne commiten gjøres fra pcen, siden det ser ut til at commitene fra
    macen ikke fungerte i det hele tatt, det har nå blitt gjort endringer i
    alle filer

--- Proglangs.js
+++ Proglangs.js
@@ -1,24 +1,17 @@
 // JavaScript Document
 
-<script>
-
-function notEmpty(){
-
-var e = document.getElementById ("field");
-var strUser = e.options[e.selectedIndex].value;
-document.getElementById('aggregator_name').innerHTML = strUser;
-
-}
-notEmpty()
-    
-    document.getElementById("field").onchange = notEmpty;
-</script>
-
-	
-<script>
-function myFunction()
+//Funksjon som henter informasjonen fra ID "myList" for å printe rett språk til sjerm når valgt//
+function programmingLanguages()
 {
-alert ("under konstruksjon")
+	//lager en funksjon og gir den verdi
+var mylist=document.getElementById("myList");
+// skriver funksjonen til Html-filen
+document.getElementById("Languages").innerHTML = mylist.options[mylist.selectedIndex].text+".";
 }
-</script>
 
+// funksjonen "myFunction" sender en alert popup melding til skjermen
+function myFunction()
+{	
+	// meldingen som gis til skjerm
+	alert ("Denne siden er under konstruksjon")
+}
